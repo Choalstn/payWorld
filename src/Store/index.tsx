6b1da6517120b0d602bdbaf74590d3d6fbc4b659
Choalstn <choalstn@gmail.com>
2023-09-05 17:45:22 +0900

    [Feat] : Redux-persist 적용

--- src/Store/index.tsx
+++ src/Store/index.tsx
@@ -1,12 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import WorkSlice from "./WorkSlice";
 
+const reducers = combineReducers({
+  work: WorkSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["work"],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    work: WorkSlice,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
 export default store;
